7066f18f213f8feb8a1d5a372a0669a3543c0778
chayanika chakraborty <2016chayanika@gmail.com>
2022-04-04 16:39:17 +0530

    responsive done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
+            <h3 className='text-white text-3xl'>Contact us on</h3>
             <h1 className='text-white'>phone:03326576870</h1>
             <h2 className='text-white'>email : 1234@shopstore.com</h2>
             
--- src/Components/Link/Link.js
+++ src/Components/Link/Link.js
@@ -3,12 +3,19 @@ import './Link.css'
 
 const Link = () => {
     return (
-        <nav id="my-nav"className='text-amber-300 p-4 my-8 md:flex justify-between text-2xl'>
-            <a href ="/Home">Home</a>
-            <a href ="/Reviews">Reviews</a>
-            <a href ="/DashBoard">DashBoard</a>
-            <a href ="/Blogs">Blogs</a>
-            <a href="/About">About</a>
+        <nav id="my-nav">
+            <ul className='text-amber-300 p-4 my-8 md:flex justify-between text-2xl'>
+                <li><a href ="/Home">Home</a></li>
+                <li><a href ="/Reviews">Reviews</a></li>
+                <li><a href ="/DashBoard">DashBoard</a></li>
+                <li><a href ="/Blogs">Blogs</a></li>
+                <li> <a href="/About">About</a></li>
+            </ul>
+            
+            
+            
+            
+           
             
         </nav>
     );
